e76931d810bd10428b5bc7ce2b05bb08bf7c1030
TheR0ck3t <60415015+TheR0ck3t@users.noreply.github.com>
2024-11-14 23:32:53 +0100

    arf

--- panel/index.js
+++ panel/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '.public')));
 app.use(cookieParser());
 
 
